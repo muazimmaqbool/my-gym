bfefd2f2847138f8776dc62f2805b0b7964da376
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-17 17:26:45 +0530

    basic component added

---
+++ Components/FrontScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FrontScreen = () => {
+  return (
+    <View>
+      <Text>FrontScreen</Text>
+    </View>
+  )
+}
+
+export default FrontScreen
+
+const styles = StyleSheet.create({})
