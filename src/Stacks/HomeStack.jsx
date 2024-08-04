2d72d59e5a450ee34dfc9de1cfdb7585e59bb875
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-08-04 17:03:35 +0530

    settings

--- src/Stacks/HomeStack.jsx
+++ src/Stacks/HomeStack.jsx
@@ -2,7 +2,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { View, Text } from 'react-native'
 import React from 'react'
 import HomeScreen from "../Screens/Home Screen/HomeScreen";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Settings from "../Screens/Settings/Settings"
+import { MaterialCommunityIcons,Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,6 +23,15 @@ const HomeStack = () => {
         options={{
           headerShown: false,
            tabBarIcon:({color})=><MaterialCommunityIcons name="home-variant-outline" size={26} color={color} />
+        }}
+        //initialParams={{user}}
+      />
+      <Tab.Screen
+        name="settings"
+        component={Settings}
+        options={{
+          headerShown: false,
+           tabBarIcon:({color})=><Feather name="settings" size={24} color={color} />
         }}
         //initialParams={{user}}
       />
