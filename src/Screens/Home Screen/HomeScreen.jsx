9c072d9b6e4eb1015ee7580f19a4732a75e51161
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-08-16 22:53:39 +0530

    Components navigation done

--- src/Screens/Home Screen/HomeScreen.jsx
+++ src/Screens/Home Screen/HomeScreen.jsx
@@ -1,51 +1,74 @@
-import { View, Text, SafeAreaView, Platform, Pressable, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  Pressable,
+  ImageBackground,
+} from "react-native";
 import React from "react";
 import { styles } from "./StyleHomeScreen";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { Feather, Ionicons, MaterialCommunityIcons, MaterialIcons, Octicons } from "@expo/vector-icons";
-import homeBg from "../../../assets/images/homeBG2.jpg"
+import {
+  Feather,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+} from "@expo/vector-icons";
+import homeBg from "../../../assets/images/homeBG2.jpg";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-    <ImageBackground source={homeBg} resizeMode="cover" style={styles.bgImage}>
-    
-      <View style={styles.topBar}>
-        <Text style={styles.gymName}>Hi-Life Fitness</Text>
-        <Text style={styles.address}>Bilal Abad Sopore</Text>
-      </View>
+      <ImageBackground
+        source={homeBg}
+        resizeMode="cover"
+        style={styles.bgImage}
+      >
+        <View style={styles.topBar}>
+          <Text style={styles.gymName}>Hi-Life Fitness</Text>
+          <Text style={styles.address}>Bilal Abad Sopore</Text>
+        </View>
 
-    
-      <View style={styles.mainView}>
-    
-        <Pressable style={styles.fieldContainer}>
-        <MaterialIcons name="dashboard" size={24} color="#48BD69" />
-          <Text style={styles.fieldText}>Dashboard Overview</Text>
-        </Pressable>
+        <View style={styles.mainView}>
+          <Pressable style={styles.fieldContainer}
+          onPress={() => navigation.navigate("dashboard")}>
+            <MaterialIcons name="dashboard" size={24} color="#48BD69" />
+            <Text style={styles.fieldText}>Dashboard Overview</Text>
+          </Pressable>
 
-        <Pressable style={styles.fieldContainer}>
-        <Ionicons name="person-add-sharp" size={24} color="#257CFF" />
-          <Text style={styles.fieldText}>New Admission</Text>
-        </Pressable>
+          <Pressable style={styles.fieldContainer}
+          onPress={() => navigation.navigate("addMember")}>
+            <Ionicons name="person-add-sharp" size={24} color="#257CFF" />
+            <Text style={styles.fieldText}>New Admission</Text>
+          </Pressable>
 
-        <Pressable style={styles.fieldContainer}>
-        <Ionicons name="people" size={24} color="#48BD69" />
-          <Text style={styles.fieldText}>All Memebers</Text>
-        </Pressable>
+          <Pressable style={styles.fieldContainer}
+          onPress={() => navigation.navigate("allMembers")}>
+            <Ionicons name="people" size={24} color="#48BD69" />
+            <Text style={styles.fieldText}>All Memebers</Text>
+          </Pressable>
 
-        <Pressable style={styles.fieldContainer}>
-        <MaterialCommunityIcons name="bell-alert" size={24} color="purple" />
-          <Text style={styles.fieldText}>Notifications/Alerts</Text>
-        </Pressable>
+          <Pressable style={styles.fieldContainer}
+          onPress={() => navigation.navigate("notifications")}>
+            <MaterialCommunityIcons
+              name="bell-alert"
+              size={24}
+              color="purple"
+            />
+            <Text style={styles.fieldText}>Notifications/Alerts</Text>
+          </Pressable>
 
-        <Pressable style={styles.fieldContainer}>
-        <Ionicons name="settings" size={24} color="#172B85" />
-          <Text style={styles.fieldText}>Settings</Text>
-        </Pressable>
-      </View>
-      
-   
-    </ImageBackground>
+          <Pressable
+            style={styles.fieldContainer}
+            onPress={() => navigation.navigate("settings")}
+          >
+            <Ionicons name="settings" size={24} color="#172B85" />
+            <Text style={styles.fieldText}>Settings</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
--- src/Stacks/HomeStack.jsx
+++ src/Stacks/HomeStack.jsx
@@ -4,6 +4,10 @@ import React from "react";
 import HomeScreen from "../Screens/Home Screen/HomeScreen";
 import Settings from "../Screens/Settings/Settings";
 import { MaterialCommunityIcons, Feather } from "@expo/vector-icons";
+import Dashboard from "../Screens/Dashboard/Dashboard"
+import AllMembers from "../Screens/All Members/AllMembers"
+import AddMember from "../Screens/New Admission/AddMember"
+import Notifcations from "../Screens/Notifications/Notifications"
 
 const Stack = createNativeStackNavigator();
 
@@ -36,10 +40,56 @@ const HomeStack = () => {
         }}
         //initialParams={{user}}
       />
+       <Stack.Screen
+        name="dashboard"
+        component={Dashboard}
+        options={{
+          headerShown: true,
+          title: "Dashboard",
+          headerShadowVisible: false,
+          
+        }}
+      />
+       <Stack.Screen
+        name="addMember"
+        component={AddMember}
+        options={{
+          headerShown: true,
+          title: "Add New Member",
+          headerShadowVisible: false,
+          
+        }}
+      />
+       <Stack.Screen
+        name="allMembers"
+        component={AllMembers}
+        options={{
+          headerShown: true,
+          title: "All Members",
+          headerShadowVisible: false,
+          
+        }}
+      />
+       <Stack.Screen
+        name="notifications"
+        component={Notifcations}
+        options={{
+          headerShown: true,
+          title: "Notifications/Alerts",
+          headerShadowVisible: false,
+          
+        }}
+      />
+
       <Stack.Screen
         name="settings"
         component={Settings}
-        //initialParams={{user}}
+        options={{
+          headerShown: true,
+          title: "Settings",
+          headerShadowVisible: false,
+          
+        }}
       />
     </Stack.Navigator>
   );
