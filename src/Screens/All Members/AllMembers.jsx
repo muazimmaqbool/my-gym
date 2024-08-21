6065382df081319212a09d6f37c36311111bd50c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-08-21 09:29:59 +0530

    text input added in all memebers section

--- src/Screens/All Members/AllMembers.jsx
+++ src/Screens/All Members/AllMembers.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { styles } from "./StyleaAllMembers";
+import { TextInput } from "react-native-paper";
 
 const AllMembers = () => {
+  const [searchText, setsearchText] = useState("");
+  console.log("Search text",searchText)
   return (
-    <View>
-      <Text>AllMembers</Text>
+    <View style={styles.container}>
+      <View>
+        <TextInput
+        value={searchText}
+        onChangeText={(e)=>setsearchText(e)}
+          mode="outlined"
+          label="Search Member"
+          placeholder="Eg: Muazim Maqbool"
+          right={<TextInput.Icon icon="account-search-outline" />}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default AllMembers
+export default AllMembers;
