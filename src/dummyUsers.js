fe92de1d7011752292748e2d4326c31509a86e8d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-09-09 09:54:57 +0530

    dummy data updted

--- src/dummyUsers.js
+++ src/dummyUsers.js
@@ -1,85 +1,105 @@
-import femaleImg from "../assets/images/Test Images/female.png"
-import maleImg from "../assets/images/Test Images/male.jpg"
-const user=[
-    {
-        first_name:"Umar",
-        last_name:"Shah",
-        address:"chinkipora, sopore-bramulla",
-        age:18,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-        first_name:"Saba",
-        last_name:"Wani",
-        address:"chinkipora, sopore-bramulla",
-        age:21,
-        gender:"Female",
-        image:femaleImg
-    },
-    {
-        first_name:"Haseeb",
-        last_name:"Shabir",
-        address:"chinkipora, sopore-bramulla",
-        age:40,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-         first_name:"Sahil",
-        last_name:"Khan",
-        address:"chinkipora, sopore-bramulla",
-        age:27,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-        first_name:"Zeenat",
-        last_name:"Khan",
-        address:"chinkipora, sopore-bramulla",
-        age:22,
-        gender:"Female",
-        image:femaleImg
-    },
-    {
-        first_name:"Rabia",
-        last_name:"Mushtaq",
-        address:"chinkipora, sopore-bramulla",
-        age:30,
-        gender:"Female",
-        image:femaleImg
-    },
-    {
-        first_name:"Bilal",
-        last_name:"Baba",
-        address:"chinkipora, sopore-bramulla",
-        age:15,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-        first_name:"Aamir",
-        last_name:"Dar",
-        address:"chinkipora, sopore-bramulla",
-        age:19,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-        first_name:"Amir",
-        last_name:"Mushtaq",
-        address:"chinkipora, sopore-bramulla",
-        age:25,
-        gender:"Male",
-        image:maleImg
-    },
-    {
-        first_name:"Insha",
-        last_name:"Bilal",
-        address:"chinkipora, sopore-bramulla",
-        age:23,
-        gender:"Female",
-        image:femaleImg
-    }
-]
-export default user
+import femaleImg from "../assets/images/Test Images/female.png";
+import maleImg from "../assets/images/Test Images/male.jpg";
+const user = [
+  {
+    first_name: "Umar",
+    last_name: "Shah",
+    address: "chinkipora, sopore-bramulla",
+    age: 18,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "01/01/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Saba",
+    last_name: "Wani",
+    address: "chinkipora, sopore-bramulla",
+    age: 21,
+    gender: "Female",
+    image: femaleImg,
+    joiningDate: "15/02/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Haseeb",
+    last_name: "Shabir",
+    address: "chinkipora, sopore-bramulla",
+    age: 40,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "20/03/2024",
+    isActive: false,
+  },
+  {
+    first_name: "Sahil",
+    last_name: "Khan",
+    address: "chinkipora, sopore-bramulla",
+    age: 27,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "10/04/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Zeenat",
+    last_name: "Khan",
+    address: "chinkipora, sopore-bramulla",
+    age: 22,
+    gender: "Female",
+    image: femaleImg,
+    joiningDate: "05/05/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Rabia",
+    last_name: "Mushtaq",
+    address: "chinkipora, sopore-bramulla",
+    age: 30,
+    gender: "Female",
+    image: femaleImg,
+    joiningDate: "17/06/2024",
+    isActive: false,
+  },
+  {
+    first_name: "Bilal",
+    last_name: "Baba",
+    address: "chinkipora, sopore-bramulla",
+    age: 15,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "08/07/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Aamir",
+    last_name: "Dar",
+    address: "chinkipora, sopore-bramulla",
+    age: 19,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "25/08/2024",
+    isActive: true,
+  },
+  {
+    first_name: "Amir",
+    last_name: "Mushtaq",
+    address: "chinkipora, sopore-bramulla",
+    age: 25,
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "10/09/2024",
+    isActive: false,
+  },
+  {
+    first_name: "Insha",
+    last_name: "Bilal",
+    address: "chinkipora, sopore-bramulla",
+    age: 23,
+    gender: "Female",
+    image: femaleImg,
+    joiningDate: "22/10/2024",
+    isActive: true,
+  },
+];
+export default user;
