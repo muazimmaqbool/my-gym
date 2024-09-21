45b920e6d425ebb0ddd8375824524a56990b604d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-09-21 22:24:42 +0530

    initial setup done

--- src/Screens/Dashboard/Dashboard.jsx
+++ src/Screens/Dashboard/Dashboard.jsx
@@ -1,13 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Avatar, Card, IconButton } from 'react-native-paper';
+import { View, Text } from "react-native";
+import React from "react";
+import { Avatar, Card, IconButton } from "react-native-paper";
+import { styles } from "./StyleDashboard";
 
 const Dashboard = () => {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <Text style={styles.gymName}>Hi-Life Fitness</Text>
+        <Text style={styles.address}>Bilal Abad Sopore</Text>
+      </View>
 
-export default Dashboard
+      <View style={styles.mainView}>
+
+      </View>
+    </View>
+  );
+};
+
+export default Dashboard;
--- src/Screens/Dashboard/StyleDashboard.js
+++ src/Screens/Dashboard/StyleDashboard.js
@@ -1,11 +1,35 @@
 import { StyleSheet } from "react-native";
 
-export const styles=StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "stretch", 
-        backgroundColor:"#FFFFFF",
-        padding:20,
-      },
-})
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: "flex-start",
+    alignItems: "stretch",
+    backgroundColor: "#3a86ff",
+  },
+  topBar: {
+    height: "20%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gymName: {
+    fontWeight: "700",
+    color: "#F9FAFB",
+    fontSize: 35,
+  },
+  address: {
+    fontWeight: "400",
+    color: "#FFFFFF",
+    fontSize: 20,
+  },
+  mainView: {
+    flex: 1,
+    backgroundColor: "white",
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    gap: 20,
+    paddingTop: 30,
+  },
+});
