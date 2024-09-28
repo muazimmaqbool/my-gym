a11ffd630583d5998bd8aa127d63dbdae5dd4185
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-09-28 14:16:11 +0530

    dummy staff users added

---
+++ src/dummyStaff.js
@@ -0,0 +1,45 @@
+import femaleImg from "../assets/images/Test Images/female.png";
+import maleImg from "../assets/images/Test Images/male.jpg";
+
+const staffUsers = [
+  {
+    first_name: "Tanveer",
+    last_name: "Ahmad",
+    address: "Sangrama, sopore",
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "01/01/2024",
+    role:"Front Desk",
+  },
+  {
+    first_name: "Shahid",
+    last_name: "Malik",
+    address: "Model Town, sopore",
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "01/01/2024",
+    role:"Trainer",
+  },
+  {
+    first_name: "Mushtaq",
+    last_name: "Bhat",
+    address: "New Colony, sopore",
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "01/01/2024",
+    role:"Trainer",
+  },
+  {
+    first_name: "Bisma",
+    last_name: "Farooq",
+    address: "New Colony, sopore",
+    gender: "Male",
+    image: maleImg,
+    joiningDate: "01/01/2024",
+    role:"Trainer",
+    
+  },
+ 
+];
+
+export default staffUsers;
