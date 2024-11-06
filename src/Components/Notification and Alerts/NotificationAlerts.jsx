73aa9a7eb36c5b43ef4ab448cebcdafad4aea814
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-06 23:04:32 +0530

    notification in dashboard added

--- src/Components/Notification and Alerts/NotificationAlerts.jsx
+++ src/Components/Notification and Alerts/NotificationAlerts.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const NotificationAlerts = () => {
   return (
-    <View>
-      <Text>NotificationAlerts</Text>
-    </View>
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+    <Text>Coming Soon</Text>
+  </View>
   )
 }
 
